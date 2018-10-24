1e1db813a6baf5a55777c3a01366809525293789
sysu-zjw <244798940@qq.com>
2018-10-25 01:47:57 +0800

    first commit

---
+++ js/Draw1.js
@@ -0,0 +1,30 @@
+/*//////////////////////////////////////////////////////////	
+//Introduction
+///////////////////////////////////////////////////////////*/
+function Draw1(){
+
+	/*First disable click event on clicker button*/
+	stopClicker();
+		
+	/*Show and run the progressBar*/
+	runProgressBar(time=700*8);
+		
+	changeTopText(newText = "Embed Knowledge to Chord Diagram",
+	loc = 4/2, delayDisappear = 0, delayAppear = 1);
+
+	changeBottomText(newText = "Jiawei ZHOU",
+	loc = 1/2, delayDisappear = 0, delayAppear = 1);
+
+	changeTopText(newText = "Let's see how to do this",
+	loc = 8/2, delayDisappear = 6, delayAppear = 8, finalText = true);
+	
+	changeBottomText(newText = "",
+	loc = 1/2, delayDisappear = 6, delayAppear = 8, finalText = true);
+	
+	//Remove arcs again
+	d3.selectAll(".arc")
+		.transition().delay(6*700).duration(2100)
+		.style("opacity", 0)
+		.each("end", function() {d3.selectAll(".arc").remove();});
+		
+};/*Draw1*/
---
+++ js/Draw12.js
@@ -0,0 +1,16 @@
+/*//////////////////////////////////////////////////////////
+//All Apple are grey
+//////////////////////////////////////////////////////////*/
+function Draw12(){
+
+	/*First disable click event on clicker button*/
+	stopClicker();
+	/*Show and run the progressBar*/
+	runProgressBar(time=700*11);	
+
+	changeTopText(newText = "One thing that stands out for Apple is that all chords are the color of grey",
+		loc = 3/2, delayDisappear = 0, delayAppear = 1, finalText = false, xloc=-80, w=210);
+	changeTopText(newText = "This means that Apple has always received more customers from other brands than they lost to them",
+		loc = 3/2, delayDisappear = 9, delayAppear = 10, finalText = true, xloc=-80, w=210);
+
+};/*Draw12*/
