ff8b22f995e758b0ba2e53fc15a411301be916c2
sysu-zjw <244798940@qq.com>
2018-10-25 13:25:22 +0800

    update

--- js/Draw1.js
+++ js/Draw1.js
@@ -15,11 +15,15 @@ function Draw1(){
 	changeBottomText(newText = "Jiawei ZHOU",
 	loc = 1/2, delayDisappear = 0, delayAppear = 1);
 
+
+	changeBottomText(newText = "",
+	loc = 1/2, delayDisappear = 3, delayAppear = 6, finalText = true);
+
+
 	changeTopText(newText = "Let's see how to do this",
 	loc = 8/2, delayDisappear = 6, delayAppear = 8, finalText = true);
 	
-	changeBottomText(newText = "",
-	loc = 1/2, delayDisappear = 6, delayAppear = 8, finalText = true);
+
 	
 	//Remove arcs again
 	d3.selectAll(".arc")
